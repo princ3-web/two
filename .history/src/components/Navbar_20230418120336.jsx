5954f716f35f2362a30fe8cc154ebf2f96f66e26
Michal Gornicki <michal.gornicki@ithouse.co>
2023-04-20 15:49:40 +0200

    another commit

---
+++ .history/src/components/Navbar_20230418120336.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import navbar from "../css/navbar.module.css";
+import logo from "../assets/images/logo.png";
+import hamburger from "../assets/images/hamburger.png";
+import hamburger from "../assets/images/hamburger.png";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const navbarButtons = [
+    { name: "Home", menu: false, items: [], link: "/" },
+    {
+      name: "Events",
+      menu: true,
+      items: ["Plan Your Day", "What's On", "Membership", "Vet Hospital", "Education", "Zoo Map"],
+      link: "/Events",
+    },
+    {
+      name: "Education",
+      menu: true,
+      items: [
+        "Read for Adventure",
+        "Educational Field Trips",
+        "Self-Guided Field Trips",
+        "Nature Explorers Preschool",
+        "Schools Out Day Camps",
+        "Spring Break Camp",
+        "Summer Camps",
+        "After Hours",
+        "Conservation Club",
+      ],
+      link: "/Education",
+    },
+    {
+      name: "Get involved",
+      menu: true,
+      items: ["What We Do", "What You Can Do", "Work at the Zoo", "Volunteer", "Sponsorship", "Conservation"],
+      link: "/Membership",
+    },
+    { name: "Animals", menu: false, items: [], link: "/Animals" },
+    { name: "News", menu: false, items: [], link: "/News" },
+  ];
+
+  const navbarTopButtons = ["About us", "Donate", "Shop"];
+
+  return (
+    <>
+      <div className={navbar.navbar}>
+        <Link className={navbar.link} to={"/"}>
+          <img className={navbar.logo} src={logo} alt="" />
+        </Link>
+        <div>
+          <div className={navbar.navbarTopButtons}>
+            {navbarTopButtons.map((item, n) => (
+              <div className={navbar.navbarTopButton} key={n}>
+                {item}
+              </div>
+            ))}
+          </div>
+          <div className={navbar.navbarButtons}>
+            {navbarButtons.map((item, n) => (
+              <div style={{ display: "block", overflow: "visible" }} key={n}>
+                <div className={navbar.navbarButton}>
+                  <Link to={item.link} style={{ textDecoration: "none", color: "unset" }}>
+                    {item.name + (item.menu ? " ⯆" : "")}
+                  </Link>
+                </div>
+
+                {item.items.length > 0 && (
+                  <div className={navbar.navbarButtonDropdown}>
+                    {item.items.map((i, n) => (
+                      <div className={navbar.navbarButtonDropdownItem} key={n}>
+                        {i}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
