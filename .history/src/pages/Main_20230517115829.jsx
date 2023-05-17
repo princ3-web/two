84209e71a8051c12a6cd0af6c5192baa72fe0289
Michal Gornicki <michal.gornicki@ithouse.co>
2023-05-17 12:13:39 +0200

    first commit

---
+++ .history/src/pages/Main_20230517115829.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import app from "../css/app.module.css";
+import Paralax from "../components/Paralax";
+import Infocard from "../components/Infocard";
+import Cards from "../components/Cards";
+import background from "../css/background.module.css";
+
+const Main = () => {
+  const openingInfos = [
+    "OPENING HOURS:",
+    "Open daily: 9.30am – 6.00am",
+    "Last admission is 5.00pm",
+  ];
+
+  const moreInfos = [
+    "Find out more about our animals",
+    "Latest media",
+    "Photos and videos",
+    "Encyclopedia of our ZOO",
+  ];
+
+  const supportInfos = [
+    "Support our ZOO",
+    "Check our subscription options",
+    "Donate our animals",
+  ];
+
+  return (
+    <>
+      <div style={{ display: "block", position: "relative" }}>
+        <Paralax
+          height={50}
+          multiplier={1}
+          background={require("../assets/images/para0.webp")}
+          object={require("../assets/images/object0.webp")}
+          right={3}
+          objectWidth={45}
+        />
+        <Infocard info={openingInfos} left={"5rem"} right={"unset"} top={"50vh"} />
+      </div>
+
+      <div className={app.section}>
+        <Cards />
+      </div>
+
+      <div style={{ display: "block", position: "relative" }}>
+        <video
+          loop
+          autoPlay
+          muted
+          className={background.video}
+          src={require("../assets/videos/" + Math.floor(Math.random() * 3 + 1) + ".mp4")}
+        />
+        <Infocard info={moreInfos} left={"unset"} right={"25rem"} top={"25rem"} />
+      </div>
+
+      <div style={{ display: "block", position: "relative" }}>
+        <Paralax
+          height={650}
+          multiplier={1}
+          background={require("../assets/images/para2.webp")}
+          object={require("../assets/images/para4.webp")}
+          right={55}
+          objectWidth={20}
+        />
+
+        <Infocard info={supportInfos} left={"unset"} right={"5rem"} top={"10rem"} />
+      </div>
+    </>
+  );
+};
+
+export default Main;
---
+++ .history/src/pages/Main_20230517121054.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import app from "../css/app.module.css";
+import Paralax from "../components/Paralax";
+import Infocard from "../components/Infocard";
+import Cards from "../components/Cards";
+import background from "../css/background.module.css";
+
+const Main = () => {
+  const openingInfos = [
+    "OPENING HOURS:",
+    "Open daily: 9.30am – 6.00am",
+    "Last admission is 5.00pm",
+  ];
+
+  const moreInfos = [
+    "Find out more about our animals",
+    "Latest media",
+    "Photos and videos",
+    "Encyclopedia of our ZOO",
+  ];
+
+  const supportInfos = [
+    "Support our ZOO",
+    "Check our subscription options",
+    "Donate our animals",
+  ];
+
+  return (
+    <>
+      <div style={{ display: "block", position: "relative" }}>
+        <Paralax
+          height={50}
+          multiplier={1}
+          background={require("../assets/images/para0.png")}
+          object={require("../assets/images/object0.png")}
+          right={3}
+          objectWidth={40}
+        />
+        <Infocard info={openingInfos} left={"5rem"} right={"unset"} top={"45vh"} />
+      </div>
+
+      <div className={app.section}>
+        <Cards />
+      </div>
+
+      <div style={{ display: "block", position: "relative" }}>
+        <video
+          loop
+          autoPlay
+          muted
+          className={background.video}
+          src={require("../assets/videos/" + Math.floor(Math.random() * 3 + 1) + ".mp4")}
+        />
+        <Infocard info={moreInfos} left={"unset"} right={"35rem"} top={"22rem"} />
+      </div>
+
+      <div style={{ display: "block", position: "relative" }}>
+        <Paralax
+          height={650}
+          multiplier={1}
+          background={require("../assets/images/para2.webp")}
+          object={require("../assets/images/object1.png")}
+          right={55}
+          objectWidth={20}
+        />
+
+        <Infocard info={supportInfos} left={"unset"} right={"5rem"} top={"10rem"} />
+      </div>
+    </>
+  );
+};
+
+export default Main;
